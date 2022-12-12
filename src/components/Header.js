f16b03af47567da4c969e40d12d5753b6fd7efd9
vinicius-oliveira-bk <vinisouza21@hotmail.com>
2022-12-12 13:50:46 -0300

    [fix] Requisito 2 - Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <>
+        <h3
+          data-testid="email-field"
+        >
+          Olá
+          { email }
+        </h3>
+        <div
+          data-testid="total-field"
+        >
+          0
+        </div>
+        <div
+          data-testid="header-currency-field"
+        >
+          BRL
+        </div>
+      </>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    const { email } = this.props;
     return (
       <>
         <h2>TrybeWallet</h2>
-        <h4>{ email }</h4>
+        <Header />
       </>
     );
   }
