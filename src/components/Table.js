f3158733187dbd679d2936853acf9f10d9c50bb3
vinicius-oliveira-bk <vinisouza21@hotmail.com>
2023-01-12 19:49:41 -0300

    [fix] Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,102 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  handleClick = (id) => {
+    const { dispatch, expenses } = this.props;
+    const array = expenses;
+    let decrease = 0;
+    expenses.forEach((element, index) => {
+      const { value, currency, exchangeRates } = element;
+      if (element.id === id) {
+        array.splice(index, 1);
+        const exchangeValue = +exchangeRates[currency].ask;
+        decrease = (+value * exchangeValue);
+      }
+    });
+    dispatch(deleteItem(array, decrease));
+  };
+
+  handleEdit = (id) => {
+    const { dispatch } = this.props;
+    dispatch(editItem(id));
+  };
+
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            expenses.map((element) => {
+              const {
+                description, tag, method, value, id, currency, exchangeRates,
+              } = element;
+              const exchangeValue = +exchangeRates[currency].ask;
+              return (
+                <tr key={ id }>
+                  <td>{ description }</td>
+                  <td>{ tag }</td>
+                  <td>{ method }</td>
+                  <td>{ (+value).toFixed(2) }</td>
+                  <td>{ exchangeRates[currency].name }</td>
+                  <td>{ exchangeValue.toFixed(2) }</td>
+                  <td>{ (+value * exchangeValue).toFixed(2) }</td>
+                  <td>Real</td>
+                  <td>
+                    <div>
+                      <button
+                        type="button"
+                        data-testid="edit-btn"
+                        onClick={ () => this.handleEdit(id) }
+                      >
+                        Editar
+                      </button>
+                      <button
+                        type="button"
+                        data-testid="delete-btn"
+                        onClick={ () => this.handleClick(id) }
+                      >
+                        Excluir
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+      currency: PropTypes.string,
+    }),
+  ).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Wallet extends React.Component {
         <h2>TrybeWallet</h2>
         <Header />
         <WalletForm />
+        <Table />
       </>
     );
   }
